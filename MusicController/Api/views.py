91c155b0d55d2bdb21de25d5bea68d3a3f0e8beb
Divyam22 <divyam2219@gmail.com>
2024-02-13 19:52:28 +0530

    Initial Project Setup

---
+++ MusicController/Api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+#initial project setup, we wont be using this in general
+def main(request):
+    return HttpResponse("Divyam2207")
